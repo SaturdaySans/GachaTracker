6debe8683ccea04a9eecae6056bf4225fba27557
Saturday <saturdaysansseven@gmail.com>
2025-02-26 16:14:30 +0800

    Update project-sekai.py

--- pages/project-sekai.py
+++ pages/project-sekai.py
@@ -5,10 +5,8 @@ import math
 def rates(crystal_count):
     three_stars = crystal_count*0.15
     four_stars = crystal_count*0.03
-    st.write(f"""
-Four Stars: {four_stars}
-Three Stars: {three_stars}
-             """)
+    st.write(f"Four Stars: {four_stars}")
+    st.write(f"Three Stars: {three_stars}")
 
 
 st.title("---Project Sekai---")
