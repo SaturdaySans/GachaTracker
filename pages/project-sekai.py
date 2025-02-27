04916b929b0a999c9772e22c74b36e56bd815acc
Saturday <saturdaysansseven@gmail.com>
2025-02-27 10:56:21 +0800

    Update project-sekai.py

--- pages/project-sekai.py
+++ pages/project-sekai.py
@@ -7,7 +7,7 @@ import math
 def rates(pulls):
     three_stars = round(pulls*0.15,2)
     four_stars = round(pulls*0.03,2)
-    featured_four_stars = four_stars * 0.4
+    featured_four_stars = round(four_stars * 0.4,2)
     st.title("Rates")
     st.write(f"Pulls: {pulls}")
     st.write(f"Featured 4 Stars: {featured_four_stars}")
