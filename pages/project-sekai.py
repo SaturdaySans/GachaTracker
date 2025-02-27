e7c113d557f9de6c8e5e21970908442b21193f7b
Saturday <saturdaysansseven@gmail.com>
2025-02-27 10:53:02 +0800

    Added Featured Four Star Calculator

--- pages/project-sekai.py
+++ pages/project-sekai.py
@@ -7,7 +7,10 @@ import math
 def rates(pulls):
     three_stars = round(pulls*0.15,2)
     four_stars = round(pulls*0.03,2)
+    featured_four_stars = four_stars * 0.4
+    st.title("Rates")
     st.write(f"Pulls: {pulls}")
+    st.write(f"Featured 4*: {featured_four_stars}")
     st.write(f"Four Stars: {four_stars}")
     st.write(f"Three Stars: {three_stars}")
     st.write(f"Leftover Crystals: {leftover_crystal}")
