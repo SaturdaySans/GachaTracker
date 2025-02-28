6abc78df6b1b0e1c96ef403e0789657fd7ff4258
Saturday <saturdaysansseven@gmail.com>
2025-02-28 16:30:11 +0800

    Update project-sekai.py

--- pages/project-sekai.py
+++ pages/project-sekai.py
@@ -23,7 +23,7 @@ def rates(pulls):
     st.write(f"4 Stars: {four_stars}")
     st.write(f"3 Stars: {three_stars}")
     st.write(f"Leftover Crystals: {leftover_crystal}")
-    st.linechart(load_data())
+    st.line_chart(load_data())
 
 
 st.title("---Project Sekai---")
